f3c599fb8d5e20dfe6361525f1a29b6df9679fad
yumii <yumiin00@hotmail.com>
2022-02-25 23:43:47 -0600

    Terminando de documentar

--- Caso_1.cpp
+++ Caso_1.cpp
@@ -230,7 +230,6 @@ string the_minion_game(string palabra){
     //puntaje[1] Stuart
     int puntaje[2]={0,0};
 
-    //BANANA
     for(int contador=0;contador<palabra.length();contador++){
         puntaje[0]+=((palabra[contador]=='A'||palabra[contador]=='E'||palabra[contador]=='I'||palabra[contador]=='O'||palabra[contador]=='U')?
                         palabra.length()-contador:0);
@@ -239,15 +238,12 @@ string the_minion_game(string palabra){
     }
 
     if(puntaje[0]>puntaje[1]){
-        //cout<<"Kevin "<<puntaje[0]<<endl;
         return "Kevin "+to_string(puntaje[0]);
     }
     else if(puntaje[0]<puntaje[1]){
-        //cout<<"Stuart "<<puntaje[1]<<endl;
         return "Stuart "+to_string(puntaje[1]);
     }
     else{
-        //cout<<"Draw"<<endl;
         return "Draw";
     }
 }
@@ -265,12 +261,12 @@ string cipher(string s, int longitud, int cantShift){
     -resultado(string): Mensaje decodificado.
    */
     string resultado;
+    int aux=0;
+
     resultado.append(s.substr(0,1));
     for(int contador=1;contador<cantShift;contador++){//Realizar el xor a los primeros digitos hasta la posicion k-1
         resultado.append(to_string(stoi(s.substr(contador,1))^stoi(s.substr(contador-1,1))));
     }
-
-    int aux=0;
 
     for (int contador1 = cantShift; contador1 < longitud; contador1++) {
         for (int contador2 = contador1-(cantShift-1); contador2 < contador1; contador2++) {
@@ -282,7 +278,19 @@ string cipher(string s, int longitud, int cantShift){
     return resultado.substr(0,longitud);
 }
 
-int pairs(vector<int> arr,int n, int k){
+int pairs(int arr[],int n, int k){
+    /*
+    Funcionamiento: Recibe un arreglo de enteros y calcula la cantidad total de parejas que cuando se restan dan
+    el valor de k (la diferencia que debe de haber entre el primer y segundo numero).
+
+    Entradas:
+    -arr(vector<int>): Arreglo de enteros para obtener la cantidad de parejas.
+    -n(int): Tamaño del arreglo.
+    -k(int): Diferencia que tiene que haber entre el primer y el segundo numero de las parejas que se formen.
+
+    Salidas:
+    -resultado(int): La cantidad de parejas
+    */
     int resultado=0;
     set<int> tabla;
     tabla.insert(arr[0]);
@@ -296,49 +304,74 @@ int pairs(vector<int> arr,int n, int k){
 }
 
 int main(){
-    //Compare the Triplets
-    /*
+    //------------------------------------Compare the Triplets-------------------------------------------------
+
     int a1[3]={1,2,3}, b1[3]={3,2,1};
     vector<int>resultado=compare_the_triplets_v1(a1,b1);
+    cout<<resultado.at(0)<<" "<<resultado.at(1)<<endl;
+    int a11[3]={6,6,6}, b11[3]={6,6,6};
+    resultado=compare_the_triplets_v1(a11,b11);
     cout<<resultado.at(0)<<" "<<resultado.at(1)<<endl;
 
     int a2[3]={17,28,30},b2[3]={99,16,8};
     resultado=compare_the_triplets_v2(a2,b2);
     cout<<resultado.at(0)<<" "<<resultado.at(1)<<endl;
-    */
+    int a22[3]={88,28,50},b22[3]={5,3,8};
+    resultado=compare_the_triplets_v2(a22,b22);
+    cout<<resultado.at(0)<<" "<<resultado.at(1)<<endl;
 
-    //Time Conversion
-    /*
+
+    //------------------------------------Time Conversion-------------------------------------------------
+
     string hora="12:01:00PM";
     cout<<endl<<time_conversion_v1(hora)<<endl;
+    hora="07:05:45PM";
+    cout<<time_conversion_v1(hora)<<endl;
 
-    hora="12:01:00AM";
-    cout<<time_conversion_v2(hora)<<endl;*/
+    hora="04:59:59AM";
+    cout<<time_conversion_v2(hora)<<endl;
+    hora="12:00:00AM";
+    cout<<time_conversion_v2(hora)<<endl;
 
-    //Subarray Division
-    /*
+    //------------------------------------Subarray Division-------------------------------------------------
+
     int s1[5]={2,2,1,3,2},d1=4,m1=2,n1=5;
     cout<<endl<<subarray_division_v1(s1,d1,m1,n1)<<endl;
+    int s11[1]={4};
+    d1=4,m1=1,n1=1;
+    cout<<subarray_division_v1(s11,d1,m1,n1)<<endl;
 
     int s2[5]={1,2,1,3,2},d2=3,m2=2,n2=5;
-    cout<<endl<<subarray_division_v2(s2,d2,m2,n2)<<endl;*/
+    cout<<subarray_division_v2(s2,d2,m2,n2)<<endl;
+    int s22[6]={1,1,1,1,1,1};
+    d2=3,m2=2,n2=6;
+    cout<<subarray_division_v2(s22,d2,m2,n2)<<endl;
 
-    //The Minion Game
-    /*
+    //------------------------------------The Minion Game-------------------------------------------------
+
     string palabra="BANANA";
-    cout<<endl<<the_minion_game(palabra)<<endl;*/
+    cout<<endl<<the_minion_game(palabra)<<endl;
+
+    palabra="BANAASA";
+    cout<<the_minion_game(palabra)<<endl;
 
 
-    //Cipher
-    /*
+    //------------------------------------Cipher-------------------------------------------------
     int n=7,k=4;
     string s="1110100110";
-    cout<<endl<<cipher(s,n,k)<<endl;*/
+    cout<<endl<<cipher(s,n,k)<<endl;
 
-    //Pairs
-    //int n=7,k=2, arr[7]={1,3,5,8,6,4,2};
-    int n=5,k=2;
-    vector<int> arr{1,5,3,4,2};
-    cout<<endl<<pairs(arr,n,k)<<endl;
+    n=17,k=3;
+    s="1010111011101000101";
+    cout<<cipher(s,n,k)<<endl;
+
+    //------------------------------------Pairs-------------------------------------------------
+    n=7,k=2;
+    int arr1[7]={1,3,5,8,6,4,2};
+    cout<<endl<<pairs(arr1,n,k)<<endl;
+
+    n=5,k=2;
+    int arr2[5]={1,5,3,4,2};
+    cout<<pairs(arr2,n,k)<<endl;
 
 }
